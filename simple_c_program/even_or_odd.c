89577d2cd2555db6c7d4dec1247b1470268fd20f
Gokul <146622216+0smic@users.noreply.github.com>
2025-06-13 16:16:13 +0530

    Create even_or_odd.c

---
+++ simple_c_program/even_or_odd.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int num = 3;
+    if (num % 2 == 0){
+        printf("Number Is Even!!");
+    }else{
+        printf("Number is Odd!!");
+    }
+    return 0;
+}
