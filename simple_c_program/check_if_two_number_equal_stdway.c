a042e9e9e1ee4f90e3b7fcc1eefe31af93dd62d2
Gokul <146622216+0smic@users.noreply.github.com>
2025-06-13 17:56:33 +0530

    Create check_if_two_number_equal_stdway.c

---
+++ simple_c_program/check_if_two_number_equal_stdway.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    
+    /* INPUT */
+    int num1, num2;
+    printf("Enter Frist Number: ");
+    scanf("%d",&num1);
+    printf("Enter Second Number: ");
+    scanf("%d",&num2);
+    
+    /* LOGIC */
+    if (num1 == num2){
+        printf("They are equal");
+    }else {
+        printf("Not equal");
+    }
+    return 0;
+
+}
